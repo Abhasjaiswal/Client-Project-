39e7bd751d76a7eeac7126b75c6ff983667d1992
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-08-26 14:34:49 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Identity from "./Components/Identity/Identity";
 import OurServices from "./Components/OurServices/Services";
 import Solutions from "./Components/Solutions/Solutions";
 import Lanyards from "./Components/Lanyards/Lanyards";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <OurServices />
       <Solutions />
       <Lanyards/>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './styles.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-section">
+          <h2>XYZ Solutions Pvt. Ltd.</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci dolorum hic ipsam. Deleniti ut neque impedit ea. Accusamus, exercitationem voluptatum molestias error ratione fugit? Voluptatem excepturi temporibus nesciunt eum nemo.
+          </p>
+        </div>
+        <div className="footer-section">
+          <h3>Quick Links</h3>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h3>Address List</h3>
+          <address>
+            Unit No-XYZ, XYZ Floor, XXXX XXXXXX XXXX,<br />
+            XXXX XXXXXX XXXXXX XXXXX,<br />
+            XXXXX Road, Andheri (E), Mumbai - 400099.<br /><br />
+            
+            <strong>Mukesh Sahoo:</strong> 9999 99999<br />
+            <strong>Email:</strong> <a href="mailto:sales.colourxpress@gmail.com">sales.xyz@gmail.com</a><br />
+            <a href="mailto:colourxpress9@gmail.com">xyzsales@gmail.com</a>
+          </address>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
