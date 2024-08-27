8f418f76602f9bdf5d6c3855783b9d9dd7e8ebb4
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-08-27 11:41:12 +0530

    changed directory structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
-import Navbar from "./Components/NavBar/Navbar";
-import Identity from "./Components/Identity/Identity";
-import OurServices from "./Components/OurServices/Services";
-import Solutions from "./Components/Solutions/Solutions";
-import Lanyards from "./Components/Lanyards/Lanyards";
-import Footer from "./Components/Footer/Footer";
-import LandingContact from "./Components/LandContact/contact"
-import OurClient from "./Components/OurClient/Client"
+// import Navbar from "./Components/NavBar/Navbar";
+// import Identity from "./Components/Identity/Identity";
+// import OurServices from "./Components/OurServices/Services";
+// import Solutions from "./Components/Solutions/Solutions";
+// import Lanyards from "./Components/Lanyards/Lanyards";
+// import Footer from "./Components/Footer/Footer";
+// import LandingContact from "./Components/LandContact/contact"
+// import OurClient from "./Components/OurClient/Client"
+import Landing from "./Pages/Landing/Lading"
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Identity />
-      <OurServices />
-      <Solutions />
-      <Lanyards/>
-      <LandingContact/>
-      <OurClient/>
-      <Footer />
+      <Landing />
     </div>
   );
 }
--- src/Pages/Landing/Lading.jsx
+++ src/Pages/Landing/Lading.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Navbar from "../../Components/Navbar/Navbar";
-import Identity from "./Components/Identity/Identity";
-import OurServices from "./Components/OurServices/Services";
-import Solutions from "./Components/Solutions/Solutions";
-import Lanyards from "./Components/Lanyards/Lanyards";
-import Footer from "./Components/Footer/Footer";
+import Navbar from "../../Components/NavBar/Navbar"
+import Identity from "../../Components/Identity/Identity";
+import OurServices from "../../Components/OurServices/Services";
+import Solutions from "../../Components/Solutions/Solutions";
+import Lanyards from "../../Components/Lanyards/Lanyards";
+import Footer from "../../Components/Footer/Footer"
+import LandingContact from "../../Components/LandContact/contact"
+import OurClient from "../../Components/OurClient/Client"
+
 
 const Landing = () => {
     return (
@@ -14,7 +17,9 @@ const Landing = () => {
         <OurServices />
         <Solutions />
         <Lanyards />
-        <Footer />
+        <LandingContact/>
+        <OurClient/>
+        <Footer/>
       </div>
     );
   };
