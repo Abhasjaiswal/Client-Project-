a155624dfac798322a996753dc71143a124b59e0
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-09-04 14:21:10 +0530

    completed Concerts page

---
+++ src/Components/WhyUsConcert/Why.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles.css';  // Import the CSS file
+
+const Why = () => {
+    return (
+        <section className="business-section3">
+            <div className="container3">
+                <div className="section-header3">
+                    <h4 className="section-subtitle3">Why Choose Us</h4>
+                    <div class="divider1"></div>
+                    <h1 className="section-title3">Empowering Business Security                    </h1>
+                </div>
+                <div className="section-content3">
+                    <p>In today's dynamic business environment, establishing a strong identity and implementing robust security measures are crucial for sustained success. At Colour Stop, we specialize in delivering tailored solutions that meet the distinct needs of businesses, empowering them to thrive amidst competition.</p>
+                    <p>Our focus extends beyond mere identification; our ID card systems are essential tools for enhancing security within organizations. Featuring advanced technologies like encrypted data and biometrics, our ID cards ensure reliable access control and protect sensitive areas of the workplace. Customizable options allow businesses to include employee details, departmental access levels, and other relevant information, enhancing management of access while safeguarding employees and assets.</p>
+                    <p>Moreover, we offer custom lanyards that not only showcase corporate branding but also foster unity among employees. Whether for everyday use with a professional design or vibrant options for special occasions, our lanyards provide a versatile solution for displaying business identity while keeping ID cards easily accessible.</p>
+                    <p>Recognizing that each business has unique security and branding needs, we provide personalized consultation and support throughout the process. From initial planning to implementation, our dedicated team collaborates closely with businesses to deliver effective security solutions that prioritize safety without compromising convenience.</p>
+                    <p>With Colour Stop's comprehensive ID card and lanyard solutions, businesses can elevate their security measures, reinforce their brand identity, and create a safer workplace environment for both employees and visitors. Partner with us today and discover how Colour Stop can redefine your business's identity and security standards.</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Why;
--- src/Pages/Services/Concert/Concert.jsx
+++ src/Pages/Services/Concert/Concert.jsx
@@ -3,12 +3,19 @@ import Navbar from "../../../Components/NavBar/Navbar";
 import ConcertHeader from "../../../Components/ConcertHeader/Header";
 import Footer from "../../../Components/Footer/Footer"
 import ConcertProduct from "../../../Components/ConcertProd/Products";
+import WhyUs from "../../../Components/WhyUsConcert/Why";
+import WhatWe from "../../../Components/WhatWeConcert/What";
+import Who from "../../../Components/WhoWeConcert/Who"
+
 const Landing = () => {
     return (
       <div>
         <Navbar />
         <ConcertHeader />
+        <WhatWe/>
         <ConcertProduct />
+        <WhyUs/>
+        <Who/>
         <Footer/>
       </div>
     );
