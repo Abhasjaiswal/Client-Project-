d3df03bb2909402b3560128108300a57b67058df
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-09-11 00:46:15 +0530

    added footer credits

---
+++ src/Components/Credits/Credits.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Credits.css';
+const Credits = () => {
+  return (
+    <div className="credits">
+      <p> © Developed by Abhas & Vanshika </p>
+    </div>
+  );
+};
+
+export default Credits;
