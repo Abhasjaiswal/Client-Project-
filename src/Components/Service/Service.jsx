77093b68e74ebc89121c05feadfd02a95af5b0b7
vanshika-verma14 <vanshika9994verma@gmail.com>
2024-10-23 18:24:05 +0530

    service

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles.css';
+import lanyardsImage from '../../assets/Images/Layer-1-1024x684.png'
+import lanyardsImage2 from '../../assets/Images/Layer-1-1024x684.png'
 
 const ExpertiseSection = () => {
   return (
@@ -36,6 +38,44 @@ const ExpertiseSection = () => {
         <div className="underline"></div>
       </div>
     </section>
+    <div className="lanyards-container">
+      <div className="lanyards-text">
+        <h2>What is Lanyards?</h2>
+        <hr className="underline" />
+        <p>
+          Lanyards are versatile accessories designed to hold badges, IDs, keys,
+          or other items securely around one's neck or wrist. Typically consisting
+          of a strip of fabric or durable material, lanyards often feature
+          attachments such as clips, hooks, or rings for easy attachment and
+          removal of items. Beyond their practical functionality, lanyards serve
+          as effective branding tools, providing businesses and organizations
+          with an opportunity to showcase their logos, slogans, or other branding
+          elements. Whether used in corporate settings, at events, or for everyday
+          convenience, lanyards offer a practical and stylish solution for carrying
+          essential items while also promoting brand visibility.
+        </p>
+      </div>
+      <div className="lanyards-image">
+        <img src={lanyardsImage} alt="Lanyards" />
+      </div>
+    </div>
+    <section className="banner-section">
+      <div className="banner-content">
+        <h1>ID Crads</h1>
+        <div className="underline"></div>
+      </div>
+    </section>
+    <div className="lanyards-container">
+      <div className="lanyards-text">
+        <h2>What is ID Cards ?</h2>
+        <hr className="underline" />
+        <p>Your identity deserves the utmost representation. At Colour Stop, our custom-designed ID cards are meticulously crafted to blend design finesse with top-notch security features, ensuring that your brand is carried with confidence at all times. Whether they’re utilized for access control, corporate identification purposes, or events, our ID cards seamlessly integrate your branding while prioritizing safety and professionalism. With attention to detail and a commitment to excellence, we ensure that each ID card not only reflects your brand identity but also serves as a reliable tool for authentication and identification. Trust Colour Stop to elevate your brand’s presence with expertly designed and secure ID cards tailored to your unique requirements.
+        </p>
+      </div>
+      <div className="lanyards-image">
+        <img src={lanyardsImage2} alt="Lanyards" />
+      </div>
+    </div>
     </>
   );
 };
